79ea1141ae432f8225c57ce3df600fa9d46e9eca
vimal Raveendran <vimalraveendra@gmail.com>
2019-09-12 21:48:45 +0200

    Updates

---
+++ precache-manifest.d6af2837ef1ecd006eb0b2606a4d0d6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70674ff84d7cd760c18b8793184472a0",
+    "url": "/weatherapp-redux/index.html"
+  },
+  {
+    "revision": "faa9fdef98d2aee4667b",
+    "url": "/weatherapp-redux/static/css/main.6f7b4e3e.chunk.css"
+  },
+  {
+    "revision": "f0235a24d6c8181c82e9",
+    "url": "/weatherapp-redux/static/js/2.7f6d6c35.chunk.js"
+  },
+  {
+    "revision": "faa9fdef98d2aee4667b",
+    "url": "/weatherapp-redux/static/js/main.4e3b9f40.chunk.js"
+  },
+  {
+    "revision": "2c0dd9d0d97e9b99e92f",
+    "url": "/weatherapp-redux/static/js/runtime~main.6ec1b665.js"
+  },
+  {
+    "revision": "cea9c9014ed270c5d050f3b8380bd59b",
+    "url": "/weatherapp-redux/static/media/cloud.cea9c901.png"
+  }
+]);
