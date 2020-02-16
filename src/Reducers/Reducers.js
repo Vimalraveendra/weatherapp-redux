ef7ede3cf9727df30787b20308d2b1da22dc0b35
vimal Raveendran <vimalraveendra@gmail.com>
2020-02-16 21:49:14 +0100

    updated Reducers

--- src/Reducers/Reducers.js
+++ src/Reducers/Reducers.js
@@ -1,47 +1,44 @@
-
 import {
-	REQUEST_WEATHER_PENDING,
-	REQUEST_WEATHER_SUCCESS,
-	REQUEST_WEATHER_FAILED
-} from '../Constants/Constants';
+  REQUEST_WEATHER_PENDING,
+  REQUEST_WEATHER_SUCCESS,
+  REQUEST_WEATHER_FAILED
+} from "../Constants/Constants";
 
-const initialState={
-	        temperature:'',
-          humidity:'',
-          city:'',
-          country:'',
-          description:'',
-          error:'',
-          isPending:false
-}
+const initialState = {
+  temperature: "",
+  humidity: "",
+  city: "",
+  country: "",
+  description: "",
+  error: "",
+  isPending: false
+};
 
+export const requestWeather = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case REQUEST_WEATHER_PENDING:
+      return {
+        ...state,
+        isPending: true
+      };
 
-export const requestWeather=(state=initialState, action={})=>{
-	const newState={...state}
-     switch(action.type){
-     	case REQUEST_WEATHER_PENDING:
-     	return {
-     		...newState,
-     		isPending:true
-     	}
-     
-           case REQUEST_WEATHER_SUCCESS:
-     	  return {
-     		...newState,
-     		  isPending:false,
-     		  temperature:action.payload.main.temp,
-          humidity:action.payload.main.humidity,
-          city:action.payload.name,
-          country:action.payload.sys.country,
-          description:action.payload.weather[0].description,
-     	}
-         case REQUEST_WEATHER_FAILED:
-            return {
-               ...newState,
-               isPending:false,
-               error:"Sorry! Please Enter a valid city and country"
-          }
-      default:
-	return newState;
-}
-}
+    case REQUEST_WEATHER_SUCCESS:
+      return {
+        ...state,
+        isPending: false,
+        temperature: action.payload.main.temp,
+        humidity: action.payload.main.humidity,
+        city: action.payload.name,
+        country: action.payload.sys.country,
+        description: action.payload.weather[0].description
+      };
+    case REQUEST_WEATHER_FAILED:
+      return {
+        ...state,
+        isPending: false,
+        error: "Sorry! Please Enter a valid city and country"
+      };
+    default:
+      return state;
+  }
+};
