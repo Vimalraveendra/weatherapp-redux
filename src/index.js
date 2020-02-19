d3779120b28688a7d19b7cf9eeeb44ac1de1c852
vimal Raveendran <vimalraveendra@gmail.com>
2020-02-19 20:43:45 +0100

    updated display of logger middleware only in the development mode

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
-import {createStore ,applyMiddleware} from 'redux';
-import {Provider} from 'react-redux';
-import { requestWeather} from './Reducers/Reducers';
-import { createLogger} from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./containers/App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { requestWeather } from "./Reducers/Reducers";
+import logger from "redux-logger";
+import thunkMiddleware from "redux-thunk";
 
-const logger = createLogger();
-const store = createStore(requestWeather,applyMiddleware(thunkMiddleware,logger));
+const middlewares = [thunkMiddleware];
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
+const store = createStore(requestWeather, applyMiddleware(...middlewares));
 
-ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
